82a5385c0267bba62ae76f8268836b14898cbe44
shohjhahon017 <shohjahonsoilov1@gmail.com>
2024-07-18 23:17:14 +0500

    ...

---
+++ js/index.js
@@ -0,0 +1,44 @@
+// If ga oid masalalar
+//1-masala
+// let age = +prompt("Yoshni kiriting");
+// if (age >= 18) {
+//   console.log("Siz katta yoshdagisiz");
+// } else {
+//   console.log("Siz hali yoshsiz");
+// }
+
+//2-masala
+// let num = +prompt("sonni kiriting");
+// if (num % 2 == 0) {
+//   console.log("Juft");
+// } else {
+//   console.log("Toq");
+// }
+
+//3-masala
+// let num = +prompt("Sonni kiriting");
+// if (num > 0) {
+//   console.log("Musbat");
+// } else {
+//   console.log("Manfiy");
+// }
+
+//4-masala
+// let temperature = +prompt("Haroratni kiriting");
+// if (temperature >= 20) {
+//   console.log("Yengil kiying");
+// } else {
+//   console.log("Qalin kiying");
+// }
+
+//5-masala
+// let point = +prompt("Ballni kiriting");
+// if (point >= 90) {
+//   console.log("A`lo");
+// } else if (point >= 70) {
+//   console.log("Yaxshi");
+// } else if (point >= 50) {
+//   console.log("Qoniqarli");
+// } else {
+//   console.log("Yomon");
+// }
